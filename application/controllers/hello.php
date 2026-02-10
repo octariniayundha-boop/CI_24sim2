cf5957cfbb3c389e066332686cb32764a996bc13
Nadia321-html <nabilak891@gmail.com>
2026-02-10 15:49:49 +0700

    latihan2
    menambahkan function di controller hello

---
+++ application/controllers/hello.php
@@ -0,0 +1,14 @@
+<?php
+
+class hello extends CI_Controller {
+    public function index()
+{
+    $data['nama'] = "Codeigneter 3 + php 7.3";
+    $this->load->view('hello_view',$data);
+}
+ public function index()
+{
+    $data['nama'] = "Codeigneter 3 + php 7.3";
+    $this->load->view('hello_view',$data);
+}
+}
